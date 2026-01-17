d61827ab16e909d8bfd1910f59945314fa1487a7
hiddenflow <69741367+hiddenflow@users.noreply.github.com>
2026-01-18 01:22:56 +0700

    Update AnimeSailPlugin.kt

--- AnimeSail/src/main/kotlin/com/animesail/AnimeSailPlugin.kt
+++ AnimeSail/src/main/kotlin/com/animesail/AnimeSailPlugin.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import com.lagradost.cloudstream3.extractors.Mp4Upload
+import com.lagradost.cloudstream3.extractors.Krakenfiles
 
 @CloudstreamPlugin
 class AnimeSailPlugin : Plugin() {
@@ -14,5 +15,6 @@ class AnimeSailPlugin : Plugin() {
         // directly.
         registerMainAPI(AnimeSail())
         registerExtractorAPI(Mp4Upload())
+        registerExtractorAPI(Krakenfiles())
     }
 }
