a66d29cac007c82f8d56be873b058dfc595538b1
hiddenflow <69741367+hiddenflow@users.noreply.github.com>
2026-01-17 21:09:10 +0700

    Update Samehadaku.kt

--- Samehadaku/src/main/kotlin/com/samehadaku/Samehadaku.kt
+++ Samehadaku/src/main/kotlin/com/samehadaku/Samehadaku.kt
@@ -9,6 +9,7 @@ import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.Qualities
 import com.lagradost.cloudstream3.utils.loadExtractor
 import com.lagradost.cloudstream3.utils.newExtractorLink
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
