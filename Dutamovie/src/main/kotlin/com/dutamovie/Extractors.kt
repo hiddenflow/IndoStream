9683c699345a9bd52f7c333c7237c08a4798561a
hiddenflow <69741367+hiddenflow@users.noreply.github.com>
2026-01-17 21:36:31 +0700

    Update Extractors.kt

--- Dutamovie/src/main/kotlin/com/dutamovie/Extractors.kt
+++ Dutamovie/src/main/kotlin/com/dutamovie/Extractors.kt
@@ -3,6 +3,10 @@ package com.dutamovie
 import com.lagradost.cloudstream3.extractors.JWPlayer
 
 
+class Ryderjet: VidhideExtractor() {
+    override var mainUrl = "https://ryderjet.com"
+}
+
 class Embedfirex : JWPlayer() {
     override var name = "embedpyrox"
     override var mainUrl = "https://embedpyrox.xyz"
