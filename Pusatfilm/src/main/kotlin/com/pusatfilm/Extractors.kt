c2d85c30a43d2c8e1762d09cf33f33f6ef41cb8a
hiddenflow <69741367+hiddenflow@users.noreply.github.com>
2026-01-17 23:04:10 +0700

    Update Extractors.kt

--- Pusatfilm/src/main/kotlin/com/pusatfilm/Extractors.kt
+++ Pusatfilm/src/main/kotlin/com/pusatfilm/Extractors.kt
@@ -1,7 +1,7 @@
 package com.pusatfilm
 
 import com.lagradost.cloudstream3.SubtitleFile
-import com.lagradost.cloudstream3.apmap
+import com.lagradost.cloudstream3.amap
 import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.base64Decode
 import com.lagradost.cloudstream3.utils.ExtractorApi
@@ -19,7 +19,7 @@ open class Kotakajaib : ExtractorApi() {
             subtitleCallback: (SubtitleFile) -> Unit,
             callback: (ExtractorLink) -> Unit
     ) {
-        app.get(url, referer = referer).document.select("ul#dropdown-server li a").apmap {
+        app.get(url, referer = referer).document.select("ul#dropdown-server li a").amap { _ ->
             loadExtractor(
                     base64Decode(it.attr("data-frame")),
                     "$mainUrl/",
