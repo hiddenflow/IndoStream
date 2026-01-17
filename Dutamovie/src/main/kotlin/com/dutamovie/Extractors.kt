3f5869f7da6ddf3829af898bdaa2dfb946885de8
hiddenflow <69741367+hiddenflow@users.noreply.github.com>
2026-01-17 21:43:23 +0700

    Update Extractors.kt

--- Dutamovie/src/main/kotlin/com/dutamovie/Extractors.kt
+++ Dutamovie/src/main/kotlin/com/dutamovie/Extractors.kt
@@ -11,13 +11,3 @@ class Embedfirex : JWPlayer() {
     override var name = "embedpyrox"
     override var mainUrl = "https://embedpyrox.xyz"
 }
-
-class Embedfirex : JWPlayer() {
-    override var name = "helvid"
-    override var mainUrl = "https://helvid.net"
-}
-
-class Embedfirex : JWPlayer() {
-    override var name = "p2pplay"
-    override var mainUrl = "https://pm21.p2pplay.pro"
-}
