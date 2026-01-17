793faaad01ac03b3434b76cdc64c5be255cc1535
hiddenflow <69741367+hiddenflow@users.noreply.github.com>
2026-01-17 23:31:22 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ subprojects {
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
     cloudstream {
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/tekuma25/Indostream")
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/hiddenflow/Indostream")
         authors = listOf("TeKuma25")
     }
 
