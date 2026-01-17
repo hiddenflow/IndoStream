f60fb4f82ba61e9474eca12422827a07f0b37505
hiddenflow <69741367+hiddenflow@users.noreply.github.com>
2026-01-17 22:44:57 +0700

    Update NodrakoridPlugin.kt

--- Nodrakorid/src/main/kotlin/com/nodrakorid/NodrakoridPlugin.kt
+++ Nodrakorid/src/main/kotlin/com/nodrakorid/NodrakoridPlugin.kt
@@ -1,7 +1,6 @@
 package com.nodrakorid
 
 import android.content.Context
-import com.lagradost.cloudstream3.extractors.Chillx
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
